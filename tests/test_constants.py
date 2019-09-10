3b212fa644f772b310268b6f52bac95806d97552
vinid <chiccobia@gmail.com>
2019-09-10 18:36:01 +0200

    tests

---
+++ tests/test_constants.py
@@ -0,0 +1,20 @@
+from pymetheus.pymetheus import LogicNet
+import pytest
+from pymetheus.utils.exceptions import DobuleInitalizationException
+class TestConstantsClass:
+
+    def test_size(self):
+        logicnet = LogicNet()
+        logicnet.constant("Rome", size=10)
+
+        assert len(logicnet.constants["Rome"]) == 10
+
+    def test_double(self):
+
+        with pytest.raises(DobuleInitalizationException):
+            logicnet = LogicNet()
+            logicnet.constant("Rome", size=10)
+            logicnet.constant("Rome", size=10)
+
+
+
---
+++ tests/test_predicates.py
@@ -0,0 +1,13 @@
+from pymetheus.pymetheus import LogicNet
+import pytest
+from pymetheus.utils.exceptions import DobuleInitalizationException
+class TestPredicatesClass:
+
+    def test_double(self):
+
+        with pytest.raises(DobuleInitalizationException):
+            logicnet = LogicNet()
+            logicnet.predicate("country", size=10)
+            logicnet.predicate("country", size=10)
+
+
